6a4c28aaed3a5f3e8cbd534491e046217d207836
Dhillyon Barrett <ed20db@leeds.ac.uk>
2023-09-06 14:13:42 +0100

    Added behaviour for when the snake sees no valid path to apple

--- snake.py
+++ snake.py
@@ -7,7 +7,7 @@ import heapq
 pygame.init()
 
 # Constants
-FPS = 10
+FPS = 40
 SCREEN_WIDTH, SCREEN_HEIGHT = 400, 400
 GRID_SIZE = 20
 GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
@@ -67,7 +67,8 @@ def main():
             
             # Snake has had no path for too long. Find valid moves until a path opens up. 
             if ticks_without_path >= MAX_TICKS_WITHOUT_PATH:
-                next_move = find_valid_move(grid, snake, snake_dir)
+                next_cell = find_best_empty_cell(grid, snake, snake_dir)
+                next_move = (next_cell[0] - snake[0][0], next_cell[1] - snake[0][1])
             
         snake_dir = next_move
         
@@ -163,43 +164,66 @@ def dijkstra(grid, start, goal, snake):
         
     return [] # No path
 
-# For use when there is no path to apple. Finds a valid empty cell to move into.
-def find_valid_move(grid, snake, snake_dir):
+# For use when there is no valid path to food. Finds best fitting neighbour cell, based on
+# longest valid empty path from snake
+def find_best_empty_cell(grid, snake, snake_dir):
     head = snake[0]
     
-    # Check if upcoming cell is valid
-    upcoming_cell = (head[0] + snake_dir[0], head[1] + snake_dir[1])
-    if (
-        0 <= upcoming_cell[0] < GRID_WIDTH
-        and 0 <= upcoming_cell[1] < GRID_HEIGHT
-        and upcoming_cell not in snake
-    ):
-        return snake_dir
+    longest_path = -1
+    best_move = (head[0] + snake_dir[0], head[1] + snake_dir[1]) # Best CURRENTLY KNOWN move
     
-    else:
-        for move in [UP, RIGHT, DOWN, LEFT]:
-            # Check if move is opposite to snakes direction
-            if (
-                (move == UP and snake_dir == DOWN)
-                or (move == DOWN and snake_dir == UP)
-                or (move == LEFT and snake_dir == RIGHT)
-                or (move == RIGHT and snake_dir == LEFT)
-            ):
-                continue
-            
-            neighbour = (head[0] + move[0], head[1] + move[1])
+    for move in [UP, RIGHT, DOWN, LEFT]:
+        neighbour = (head[0] + move[0], head[1] + move[1])
+        
+        # Check if neighbour is valid
+        if (
+            0 <= neighbour[0] < GRID_WIDTH
+            and 0 <= neighbour[1] < GRID_WIDTH
+            and (neighbour[0], neighbour[1]) not in snake
+        ):
+            path_length = calculate_potential_path_length(snake, grid, neighbour)
+            if path_length > longest_path:
+                longest_path = path_length
+                best_move = neighbour
+    
+    # Returns best fitting neighbour cell
+    return best_move           
+
+# A modified version of the Dijkstra function which returns the length of the
+# longest empty path from the snake
+def calculate_potential_path_length(grid, snake, start):
+    # Init fringe set with start node
+    fringe_set = [(0, start)]
+    heapq.heapify(fringe_set)
+    
+    # Init costs
+    cost = {(x, y): float('inf') for x in range(GRID_WIDTH) for y in range(GRID_HEIGHT)}
+    cost[start] = 0
+    
+    potential_path_length = 0
+    
+    while fringe_set:
+        _, current = heapq.heappop(fringe_set)
+        
+        for move in (UP, RIGHT, DOWN, LEFT):
+            neighbour = (current[0] + move[0], current[1] + move[1])
             
             # Check if neighbour is valid
             if (
                 0 <= neighbour[0] < GRID_WIDTH
-                and 0 <= neighbour[1] < GRID_HEIGHT
-                and grid[neighbour[0]][neighbour[1]] is None
+                and 0 <= neighbour[1] < GRID_WIDTH
+                and (neighbour[0], neighbour[1]) not in snake
             ):
-                return move
-            else:
-                continue
+                tentative_cost = cost[current] + 1
+                
+                if tentative_cost < cost[neighbour]:
+                    cost[neighbour] = tentative_cost
+                    heapq.heappush(fringe_set, (cost[neighbour], neighbour))
+                
+        if cost[current] > potential_path_length:
+            potential_path_length = cost[current]
     
-    return snake_dir # No suitable move
+    return potential_path_length
 
 # Self explanatory
 def update_grid(grid, x, y, value):
